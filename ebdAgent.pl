5430a1556737e4591fd25349d401ad1b938dfac7
root <root@ebd36.ebdcloud.intern>
2014-01-07 20:12:56 +0100

    Overrriding the /etc/hosts perfectly

--- ebdAgent.pl
+++ ebdAgent.pl
@@ -6,7 +6,7 @@
 
 use strict;
 use warnings;
-use YAML qw(LoadFile);
+#use YAML qw(LoadFile);
 use Data::Dumper;
 use LWP::UserAgent;
 use LWP::UserAgent::DNS::Hosts;
@@ -31,9 +31,7 @@ sub check_global {
  		eval{
 			my $response = $ua->get("http://$_/");
  
-			if ($response->is_success) {
-			#	print $response->decoded_content;  # or whatever
- 			}
+			if ($response->is_success) {}
  			else {
      				die $response->status_line;
  			}
@@ -44,9 +42,34 @@ sub check_global {
 		
 	}
 };
+sub check_local {
+        #my $ua = LWP::UserAgent->new;
+        #$ua->timeout(10);
+        foreach (@DOMAINS) {
+                chomp($_);
+		LWP::UserAgent::DNS::Hosts->register_host("$_" => '127.0.0.1',);
+		LWP::UserAgent::DNS::Hosts->enable_override;
+		my $ua = LWP::UserAgent->new;
+		$ua->timeout(10);
+                eval{
+                        my $response = $ua->get("http://$_:8080");
+
+                        if ($response->is_success) {
+                                #next;
+                        }
+                        else {
+                                die $response->status_line;
+                        }
+                };
+                if ($@) {
+                        warn $@;
+                }
+
+        }
+}; 
 
 ################BEGIN##################
 
 init();
 check_global;
-#check_local;
+check_local;
