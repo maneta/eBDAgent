f07b67773b6e39aad69c705bdc768e75e2118d84
root <root@ebd36.ebdcloud.intern>
2014-01-07 18:42:07 +0100

    get state from gloabl via proxy website

--- ebdAgent.pl
+++ ebdAgent.pl
@@ -8,21 +8,45 @@ use strict;
 use warnings;
 use YAML qw(LoadFile);
 use Data::Dumper;
+use LWP::UserAgent;
+use LWP::UserAgent::DNS::Hosts;
 
+my @DOMAINS;
 
 sub init {
-##Read The Domain List for Monitorize 
+##Read The Domain List from a file. 
 
-	
-	my @domains;
-	eval {
-		@domains = LoadFile("domains.yml");
-	};
-	warn $@ if $@;
-
-	print Dumper(@domains);
+	my $filename = 'domains.dat';
+	open(my $fh, '<', $filename)
+	  or die "Could not open file '$filename' $!";
+	@DOMAINS = <$fh>;
+	close $fh;
 }
+
+sub check_global {
+	my $ua = LWP::UserAgent->new;
+ 	$ua->timeout(10);
+ 	foreach (@DOMAINS) {
+		chomp($_);
+ 		eval{
+			my $response = $ua->get("http://$_/");
+ 
+			if ($response->is_success) {
+			#	print $response->decoded_content;  # or whatever
+ 			}
+ 			else {
+     				die $response->status_line;
+ 			}
+		}; 
+		if ($@) {
+			warn $@;
+		}
+		
+	}
+};
+
 ################BEGIN##################
 
 init();
-
+check_global;
+#check_local;
