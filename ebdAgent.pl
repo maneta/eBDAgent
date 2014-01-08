0fb81a0e190295744248c8aa8577341a7bf4c137
root <root@ebd36.ebdcloud.intern>
2014-01-08 12:28:21 +0100

    getting the YAML config file Right

--- ebdAgent.pl
+++ ebdAgent.pl
@@ -6,29 +6,42 @@
 
 use strict;
 use warnings;
-#use YAML qw(LoadFile);
+use YAML qw(LoadFile);
 use Data::Dumper;
 use LWP::UserAgent;
 use LWP::UserAgent::DNS::Hosts;
 
-my @DOMAINS;
+my $CONFIG;
 
-sub init {
-##Read The Domain List from a file. 
+my $CONFIG_FILE = "agent_conf.yml";
 
-	my $filename = 'domains.dat';
-	open(my $fh, '<', $filename)
-	  or die "Could not open file '$filename' $!";
-	@DOMAINS = <$fh>;
-	close $fh;
+sub load_config {
+##Read The Configuration from a YAML file. 
+
+	$CONFIG = LoadFile("$CONFIG_FILE") or die "Missing $CONFIG_FILE File";
+	#ToDo: Make a Hash to check the Configuration Parameters Like
+=pod
+	my $error = 0;
+        for my $arg (sort keys %CONFIG_ARGS) {
+                if (!$CONFIG_ARGS{$arg} && ! $CONFIG->{$arg}) {
+                                warn "Missing configuration for $arg in $FILE_CONFIG\n";
+                                $error++;
+                }
+                ${$CONFIG_ARGS{$arg}} = $CONFIG->{$arg}        if $CONFIG->{$arg};
+        warn "ERROR: Paràmetre $arg desconegut a $FILE_CONFIG\n"
+            if !exists $CONFIG_ARGS{$arg};
+        }
+        exit -1 if $error;
+=cut
 }
 
 sub check_global {
+	my $domains = $CONFIG->{domains};
 	my $ua = LWP::UserAgent->new;
  	$ua->timeout(10);
- 	foreach (@DOMAINS) {
-		chomp($_);
- 		eval{
+	$ua->agent('eBDAgent/1.0');
+ 	foreach (@$domains) {
+		eval{
 			my $response = $ua->get("http://$_/");
  
 			if ($response->is_success) {}
@@ -38,24 +51,24 @@ sub check_global {
 		}; 
 		if ($@) {
 			warn $@;
+			#ToDo: Create a Hash with the Report Info
 		}
-		
 	}
 };
 sub check_local {
-        #my $ua = LWP::UserAgent->new;
-        #$ua->timeout(10);
-        foreach (@DOMAINS) {
-                chomp($_);
+	my $domains = $CONFIG->{domains};
+        foreach (@$domains) {
 		LWP::UserAgent::DNS::Hosts->register_host("$_" => '127.0.0.1',);
 		LWP::UserAgent::DNS::Hosts->enable_override;
 		my $ua = LWP::UserAgent->new;
 		$ua->timeout(10);
+		$ua->agent('eBDAgent/1.0');
                 eval{
                         my $response = $ua->get("http://$_:8080");
 
                         if ($response->is_success) {
-                                #next;
+                                print $response->decoded_content;
+				#next;
                         }
                         else {
                                 die $response->status_line;
@@ -63,13 +76,13 @@ sub check_local {
                 };
                 if ($@) {
                         warn $@;
+			#ToDo: Create a Hash with the Report Info
                 }
-
         }
 }; 
 
 ################BEGIN##################
 
-init();
+load_config();
 check_global;
 check_local;
